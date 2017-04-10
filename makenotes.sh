0b835fcb4af4d0a8071a5377e6b70bd842087c3e
luthes <steve.luther@gmail.com>
2017-04-10 15:57:28 -0700

    fixed a variable

--- makenotes.sh
+++ makenotes.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 # Just a small script to make my notes for class.
-#
-#
-#
 #touch $(echo Morality.`date "+%Y%m%d"`)
 DATE=`date "+%Y%m%d"`
 if [ $# -ne 4 ]; then
@@ -29,7 +26,9 @@ elif [ $# -eq 4 ]; then
 #    echo LECTURE = "${LECTURE}"
 #    echo DATE = "$DATE"
     mkdir -p ${CLASS}
-    touch ./${CLASS}/${LECTURE}.${DATE}
+    FILE=./${CLASS}/${LECTURE}.${DATE}
+    touch ${FILE}
+    echo "# ${CLASS} -- ${LECTURE} -- ${DATE}" >> ${FILE}
 #TODO: Check if we're making a duplicate, if so, add time.
-    echo "Created file: ./${CLASS}/${LECTURE}.${DATE}"
+    echo "Created file: ${FILE}" 
 fi
